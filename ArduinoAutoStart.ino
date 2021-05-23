2fc0a6b20ae14baa312bc1a57ed20af677af381f
kygm <rickev20@gmail.com>
2021-05-23 13:48:01 -0500

    V 1.02

--- ArduinoAutoStart.ino
+++ ArduinoAutoStart.ino
@@ -4,39 +4,45 @@
  * set to function with a 433mhz transmitter
  * and reciever, just like the standard 
  * chevy car auto start system.
+ * 
+ * Date: 5/9/21
+ * Author: KYGM
  */
  
-
-#include <RH_ASK.h>
 #include<SPI.h>
 
 //instance variables
-int starterRelay = 1;
-int led = 2;
+int starterRelay = 4;
+int led = 3;
 int neutralSwitch = 3;
 //clutch switch only for manual 
 //cars, set to HIGH in loop function
-int clutchRelay = 4;
+int clutchRelay = 0;
 //relay to ignition switch to 
 //close ignition circuit and
 //start fuel pump and ecu
-int ignitionRelay = 5;
+int ignitionRelay = 1;
 //setting engine input switch
-// as analog input
+//as analog input
 int engineStarted = A0;
-int startRemote = 6;
+int reciever = 5;
 
-RH_ASK rf_driver;
+/*
+ * Avoiding neutral switch as it 
+ * may be unnescessary for automatic
+ * vehicles. To implement, add if block
+ * to handle neutral high reading from
+ * switch.
+ */
+
 void setup() 
 {
-  //initializing rf driver
-  rf_driver.init();
   pinMode(starterRelay, OUTPUT);
   pinMode(ignitionRelay, OUTPUT);
   pinMode(led, OUTPUT);
   pinMode(neutralSwitch, INPUT);
   pinMode(clutchRelay, OUTPUT);
-  pinMode(startRemote, INPUT);
+  pinMode(reciever, INPUT);
   //setting baud rate
   Serial.begin(9600);
 }
@@ -47,38 +53,90 @@ int voltage = 500;
 void loop() 
 {
  
-  //if reciever recieved start signal
-  if(digitalRead(startRemote) == HIGH);
+  //if reciever start signal
+  //check engine run status
+  if(digitalRead(reciever) == HIGH);
   {
     //if engine is not running, begin start sequence
     if(analogRead(engineStarted) < voltage)
     {
-      //if not in neutral, DO NOT START
-      if(digitalRead(neutralSwitch) == LOW)
+      Serial.println("Starting Car Operations");
+      //opening ignition switch
+      digitalWrite(ignitionRelay, HIGH);
+      //wait 2 seconds for fuel pump
+      fuelPumpLight();
+      delay(2000);
+      //send power to starter relay
+      //for 3 seconds to crank car
+      runLed(true);
+      digitalWrite(starterRelay, HIGH);
+      delay(3000);
+      digitalWrite(starterRelay, LOW);
+      runLed(false);
+
+      if(analogRead(engineStarted) < voltage)
       {
-        Serial.println("Starting Car Operations");
-        //opening ignition switch
-        digitalWrite(ignitionRelay, HIGH);
-        //wait 2 seconds for fuel pump
-        delay(2000);
-        //send power to starter relay
-        //for 3 seconds to crank car
-        digitalWrite(starterRelay, HIGH);
-        delay(3000);
-        digitalWrite(starterRelay, LOW);
-        Serial.println("Completed Car Start Operations");
+        Serial.println("Error Starting Engine!");
+        runError();
       }
-      //else dont start engine
       else
       {
-        Serial.println("Car not in neutral!");
+        Serial.println("Completed Car Start Operations");
       }
+
     }
     //engine is already running
     else
     {
       Serial.println("Engine is running!");
+      //let user know that engine is running
+      engineRunning();
     }
   }//end start remote if block
   
+}
+
+//led functions
+void engineRunning()
+{
+  //blink led 3 times to let user
+  //know that engine is running
+  
+  int bk = 3;
+  for(int x = 0; x < bk; x++)
+  {
+    digitalWrite(led, HIGH);
+    delay(700);
+    digitalWrite(led, LOW);
+  }
+}
+void fuelPumpLight()
+{
+  digitalWrite(led, HIGH);
+  delay(700);
+  digitalWrite(led, LOW);
+}
+void runError()
+{
+  //blink led 5 times to let user
+  //know that there is an error
+  
+  int bk = 5;
+  for(int x = 0; x < bk; x++)
+  {
+    digitalWrite(led, HIGH);
+    delay(700);
+    digitalWrite(led, LOW);
+  }
+}
+void runLed(bool on)
+{
+  if(on)
+  {
+    digitalWrite(led, HIGH);
+  }
+  else
+  {
+    digitalWrite(led, LOW);
+  }
 }
